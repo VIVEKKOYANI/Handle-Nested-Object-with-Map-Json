c27256361a30bf4564b6032fe7c67e27aa4297b8
vivek <vivek.koyani@gmail.com>
2020-03-17 23:07:54 +0530

    add json data

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      data: [
+        {
+          name: 'Anil',
+          details: [
+            {info: '8888'},
+            {info: 'anil@gmail.com'},
+            {info: 'noida'}
+          ]
+        },
+        {
+          name: 'Same',
+          details: [
+            {info: '7777'},
+            {info: 'same@gmail.com'},
+            {info: 'delhi'}
+          ]
+        },
+        {
+          name: 'Peter',
+          details: [
+            {info: '6666'},
+            {info: 'peter@gmail.com'},
+            {info: 'gurugram'}
+          ]
+        },
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
